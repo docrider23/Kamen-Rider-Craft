1fab7ec2ec081ae3d29cba9ee891f86db33a0736
kelcok22 <kelcok22@live.co.uk>
2024-06-10 17:25:49 +0100

    belts not finished

--- src/main/java/com/kelco/kamenridercraft/Items/hibiki/ArmedSaberItem.java
+++ src/main/java/com/kelco/kamenridercraft/Items/hibiki/ArmedSaberItem.java
@@ -9,7 +9,6 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.level.Level;
