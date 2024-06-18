7fa745c8a1d20dff9aed126abb801459f5f03f50
LiasDan <liasdan22@gmail.com>
2024-06-19 03:08:37 +0800

    AntWrestler and BurningGorilla

--- src/main/java/com/kelco/kamenridercraft/Items/Gotchard_Rider_Items.java
+++ src/main/java/com/kelco/kamenridercraft/Items/Gotchard_Rider_Items.java
@@ -38,12 +38,19 @@ public class Gotchard_Rider_Items {
     public static final RegistryObject<Item> HOPPER1_RIDE_CHEMY_CARD = ITEMS.register("hopper1_ride_chemy_card",
             () -> new RiderFormChangeItem(new Item.Properties(),0,"","gotchard","gotchardriver_belt",
             		new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 1,true,false),
+            		new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 1,true,false),
             		new MobEffectInstance(MobEffects.JUMP, 40, 1,true,false)).AddToTabList(RiderTabs.GOTCHARD_TAB_ITEM));
     /* pikahotaru
      * gengenchoucho
      * bakuonzemi
-     * antrooper
-     * greatonbo
+     */
+    
+    public static final RegistryObject<Item> ANTROOPER_RIDE_CHEMY_CARD = ITEMS.register("antrooper_ride_chemy_card",
+            () -> new RiderFormChangeItem(new Item.Properties(),0,"_ant_wrestler","gotchard","gotchardriver_belt",
+            		new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 2,true,false),
+            		new MobEffectInstance(Effect_core.PUNCH.get(), 40, 2,true,false)).AddToTabList(RiderTabs.GOTCHARD_TAB_ITEM));
+    
+    /* greatonbo
      * stagvine
      * kaiserbee
      * kamantis
@@ -53,15 +60,21 @@ public class Gotchard_Rider_Items {
      * dokkirimajin
      * doctorkozo
      * pilets
-     * wrestler_g
-     * sasukemaru
+     */
+    
+    public static final RegistryObject<Item> WRESTLER_G_RIDE_CHEMY_CARD = ITEMS.register("wrestler_g_ride_chemy_card",
+            () -> new RiderFormChangeItem(new Item.Properties(),0,"_ant_wrestler","gotchard","gotchardriver_belt",
+            		new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 2,true,false),
+            		new MobEffectInstance(Effect_core.PUNCH.get(), 40, 2,true,false)).AddToTabList(RiderTabs.GOTCHARD_TAB_ITEM));
+    
+     /* sasukemaru
      * bulletbaang
      */
     
     public static final RegistryObject<Item> APPAREBUSHIDO_RIDE_CHEMY_CARD = ITEMS.register("apparebushido_ride_chemy_card",
             () -> new RiderFormChangeItem(new Item.Properties(),0,"_appare_skebow","gotchard","gotchardriver_belt",
-            		new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 3,true,false),
-            		new MobEffectInstance(MobEffects.JUMP, 40, 3,true,false),
+            		new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 2,true,false),
+            		new MobEffectInstance(MobEffects.JUMP, 40, 2,true,false),
             		new MobEffectInstance(Effect_core.SLASH.get(), 40, 2,true,false))
             .ChangeModel("geo/gotchard_appare_skebow.geo.json").AddToTabList(RiderTabs.GOTCHARD_TAB_ITEM));
     
@@ -73,8 +86,8 @@ public class Gotchard_Rider_Items {
     
     public static final RegistryObject<Item> SKEBOWS_RIDE_CHEMY_CARD = ITEMS.register("skebows_ride_chemy_card",
             () -> new RiderFormChangeItem(new Item.Properties(),0,"_appare_skebow","gotchard","gotchardriver_belt",
-            		new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 3,true,false),
-            		new MobEffectInstance(MobEffects.JUMP, 40, 3,true,false),
+            		new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 2,true,false),
+            		new MobEffectInstance(MobEffects.JUMP, 40, 2,true,false),
             		new MobEffectInstance(Effect_core.SLASH.get(), 40, 2,true,false))
             .ChangeModel("geo/gotchard_appare_skebow.geo.json").AddToTabList(RiderTabs.GOTCHARD_TAB_ITEM));
     
@@ -84,7 +97,7 @@ public class Gotchard_Rider_Items {
     public static final RegistryObject<Item> GEKIOCOPTER_RIDE_CHEMY_CARD = ITEMS.register("gekiocopter_ride_chemy_card",
             () -> new RiderFormChangeItem(new Item.Properties(),0,"","valvarad","",
             		new MobEffectInstance(Effect_core.FLYING.get(), 40, 1,true,false),
-            		new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 1,true,false))
+            		new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 2,true,false))
             .ChangeSlot(2).addSwitchForm(Modded_item_core.BLANK_FORM.get()).AddToTabList(RiderTabs.GOTCHARD_TAB_ITEM));
     
     public static final RegistryObject<Item> DEEPMARINER_RIDE_CHEMY_CARD = ITEMS.register("deepmariner_ride_chemy_card",
@@ -103,12 +116,14 @@ public class Gotchard_Rider_Items {
     
     public static final RegistryObject<Item> GUTSSHOVEL_RIDE_CHEMY_CARD = ITEMS.register("gutsshovel_ride_chemy_card",
             () -> new RiderFormChangeItem(new Item.Properties(),0,"","valvarad","",
-            		new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 2,true,false))
+            		new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 3,true,false),
+            		new MobEffectInstance(Effect_core.PUNCH.get(), 40, 3,true,false))
             .ChangeSlot(3).addSwitchForm(Modded_item_core.BLANK_FORM.get()).AddToTabList(RiderTabs.GOTCHARD_TAB_ITEM));
     
     public static final RegistryObject<Item> STEAMLINER_RIDE_CHEMY_CARD = ITEMS.register("steamliner_ride_chemy_card",
             () -> new RiderFormChangeItem(new Item.Properties(),0,"","gotchard","gotchardriver_belt",
             		new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 1,true,false),
+            		new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 1,true,false),
             		new MobEffectInstance(MobEffects.JUMP, 40, 1,true,false)).AddToTabList(RiderTabs.GOTCHARD_TAB_ITEM));
 	
     /* exceedfighter
@@ -120,8 +135,15 @@ public class Gotchard_Rider_Items {
      * bountybunny
      * hawkstar
      * tsupparihebi
-     * gorillasensei
-     * ganvhale
+     */
+    
+    public static final RegistryObject<Item> GORILLASENSEI_RIDE_CHEMY_CARD = ITEMS.register("gorillasensei_ride_chemy_card",
+            () -> new RiderFormChangeItem(new Item.Properties(),0,"_burning_gorilla","gotchard","gotchardriver_belt",
+            		new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 4,true,false),
+            		new MobEffectInstance(Effect_core.PUNCH.get(), 40, 4,true,false),
+            		new MobEffectInstance(Effect_core.FIRE_PUNCH.get(), 40, 4,true,false)).AddToTabList(RiderTabs.GOTCHARD_TAB_ITEM));
+    
+    /* ganvhale
      * lixion
      * 
      * raidenji
@@ -136,8 +158,15 @@ public class Gotchard_Rider_Items {
      * x_fortress
      * 
      * happyclover
-     * burningnero
-     * bambamboo
+     */
+    
+    public static final RegistryObject<Item> BURNINGNERO_RIDE_CHEMY_CARD = ITEMS.register("burningnero_ride_chemy_card",
+            () -> new RiderFormChangeItem(new Item.Properties(),0,"_burning_gorilla","gotchard","gotchardriver_belt",
+            		new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 4,true,false),
+            		new MobEffectInstance(Effect_core.PUNCH.get(), 40, 4,true,false),
+            		new MobEffectInstance(Effect_core.FIRE_PUNCH.get(), 40, 4,true,false)).AddToTabList(RiderTabs.GOTCHARD_TAB_ITEM));
+    
+    /* bambamboo
      * saboneedle
      */
     
