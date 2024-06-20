f9ab54188c6e6d15aab23496c1bd27655d5661e1
LiasDan <liasdan22@gmail.com>
2024-06-21 07:40:11 +0800

    FULL FORCE

--- src/main/java/com/kelco/kamenridercraft/Items/Kabuto_Rider_Items.java
+++ src/main/java/com/kelco/kamenridercraft/Items/Kabuto_Rider_Items.java
@@ -41,7 +41,7 @@ public class Kabuto_Rider_Items {
                 () -> new BaseItem(new Item.Properties()).AddToTabList(RiderTabs.KABUTO_TAB_ITEM));
         
         public static final RegistryObject<Item> KABUTO_ZECTER_MASK = ITEMS.register("kabuto_zecter_mask",
-                () -> new RiderFormChangeItem(new Item.Properties(),0,"_mask","kabuto","kabuto_rider_belt",
+                () -> new RiderFormChangeItem(new Item.Properties(),0,"_masked","kabuto","kabuto_rider_belt_m",
                         new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 1,true,false),
                         new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 40, 0,true,false)));
         public static final RegistryObject<Item> KABUTO_ZECTER = ITEMS.register("kabuto_zecter",
@@ -50,37 +50,37 @@ public class Kabuto_Rider_Items {
                         new MobEffectInstance(MobEffects.DIG_SPEED, 40, 3,true,false)).addSwitchForm(KABUTO_ZECTER_MASK.get()).AddToTabList(RiderTabs.KABUTO_TAB_ITEM));
 
         public static final RegistryObject<Item> THEBEE_ZECTER_MASK = ITEMS.register("thebee_zecter_mask",
-                () -> new RiderFormChangeItem(new Item.Properties(),0,"_mask","thebee","thebee_rider_belt",
+                () -> new RiderFormChangeItem(new Item.Properties(),0,"_masked","thebee","zect_buckle_belt",
                         new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 1,true,false),
                         new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 40, 0,true,false)));
         public static final RegistryObject<Item> THEBEE_ZECTER = ITEMS.register("thebee_zecter",
-                () -> new RiderFormChangeItem(new Item.Properties(),0,"","thebee","thebee_rider_belt",
+                () -> new RiderFormChangeItem(new Item.Properties(),0,"","thebee","zect_buckle_belt",
                         new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 3,true,false),
                         new MobEffectInstance(MobEffects.DIG_SPEED, 40, 3,true,false),
                         new MobEffectInstance(MobEffects.JUMP, 40, 4,true,false)).addSwitchForm(THEBEE_ZECTER_MASK.get()).AddToTabList(RiderTabs.KABUTO_TAB_ITEM));
 
         public static final RegistryObject<Item> DRAKE_ZECTER_MASK = ITEMS.register("drake_zecter_mask",
-                () -> new RiderFormChangeItem(new Item.Properties(),0,"_mask","drake","drake_rider_belt",
+                () -> new RiderFormChangeItem(new Item.Properties(),0,"_masked","drake","zect_buckle_belt",
                         new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 1,true,false),
                         new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 40, 0,true,false)));
         public static final RegistryObject<Item> DRAKE_ZECTER = ITEMS.register("drake_zecter",
-                () -> new RiderFormChangeItem(new Item.Properties(),0,"","drake","drake_rider_belt",
+                () -> new RiderFormChangeItem(new Item.Properties(),0,"","drake","zect_buckle_belt",
                         new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 3,true,false),
                         new MobEffectInstance(MobEffects.DIG_SPEED, 40, 3,true,false),
                         new MobEffectInstance(MobEffects.NIGHT_VISION, 400, 0,true,false)).addSwitchForm(DRAKE_ZECTER_MASK.get()).AddToTabList(RiderTabs.KABUTO_TAB_ITEM));
 
         public static final RegistryObject<Item> SASWORD_ZECTER_MASK = ITEMS.register("sasword_zecter_mask",
-                () -> new RiderFormChangeItem(new Item.Properties(),0,"_mask","sasword","sasword_rider_belt",
+                () -> new RiderFormChangeItem(new Item.Properties(),0,"_masked","sasword","zect_buckle_belt",
                         new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 1,true,false),
                         new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 40, 0,true,false)));
         public static final RegistryObject<Item> SASWORD_ZECTER = ITEMS.register("sasword_zecter",
-                () -> new RiderFormChangeItem(new Item.Properties(),0,"","sasword","sasword_rider_belt",
+                () -> new RiderFormChangeItem(new Item.Properties(),0,"","sasword","zect_buckle_belt",
                         new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 3,true,false),
                         new MobEffectInstance(MobEffects.DIG_SPEED, 40, 3,true,false),
                         new MobEffectInstance(MobEffects.DAMAGE_BOOST, 400, 2,true,false)).addSwitchForm(SASWORD_ZECTER_MASK.get()).AddToTabList(RiderTabs.KABUTO_TAB_ITEM));
 
         public static final RegistryObject<Item> GATACK_ZECTER_MASK = ITEMS.register("gatack_zecter_mask",
-                () -> new RiderFormChangeItem(new Item.Properties(),0,"_mask","gatack","gatack_rider_belt",
+                () -> new RiderFormChangeItem(new Item.Properties(),0,"_masked","gatack","gatack_rider_belt_m",
                         new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 1,true,false),
                         new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 40, 0,true,false)));
         public static final RegistryObject<Item> GATACK_ZECTER = ITEMS.register("gatack_zecter",
@@ -98,14 +98,14 @@ public class Kabuto_Rider_Items {
                 new MobEffectInstance(MobEffects.NIGHT_VISION, 400, 0,true,false)));
 
         public static final RegistryObject<Item> KICKHOPPER_ZECTER = ITEMS.register("kickhopper_zecter",
-                () -> new RiderFormChangeItem(new Item.Properties(),0,"","kickhopper","kickhopper_rider_belt",
+                () -> new RiderFormChangeItem(new Item.Properties(),0,"","kickhopper","kickhopper_zect_buckle_belt",
                         new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 2,true,false),
                         new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 3,true,false),
                         new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 3,true,false),
                         new MobEffectInstance(MobEffects.DIG_SPEED, 40, 4,true,false),
                         new MobEffectInstance(MobEffects.JUMP, 40, 7,true,false)).AddToTabList(RiderTabs.KABUTO_TAB_ITEM));
         public static final RegistryObject<Item> PUNCHHOPPER_ZECTER = ITEMS.register("punchhopper_zecter",
-                () -> new RiderFormChangeItem(new Item.Properties(),0,"","punchhopper","puchhopper_rider_belt",
+                () -> new RiderFormChangeItem(new Item.Properties(),0,"","punchhopper","punchhopper_zect_buckle_belt",
                         new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 2,true,false),
                         new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 3,true,false),
                         new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 4,true,false),
@@ -113,7 +113,7 @@ public class Kabuto_Rider_Items {
                         new MobEffectInstance(MobEffects.JUMP, 40, 4,true,false)).AddToTabList(RiderTabs.KABUTO_TAB_ITEM));
 
         public static final RegistryObject<Item> DARK_KABUTO_ZECTER_MASK = ITEMS.register("dark_kabuto_zecter_mask",
-                () -> new RiderFormChangeItem(new Item.Properties(),0,"_mask","dark_kabuto","dark_kabuto_rider_belt",
+                () -> new RiderFormChangeItem(new Item.Properties(),0,"_masked","dark_kabuto","dark_kabuto_rider_belt_m",
                         new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 1,true,false),
                         new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 40, 0,true,false)));
         public static final RegistryObject<Item> DARK_KABUTO_ZECTER = ITEMS.register("dark_kabuto_zecter",
@@ -130,11 +130,11 @@ public class Kabuto_Rider_Items {
                         new MobEffectInstance(MobEffects.NIGHT_VISION, 400, 0,true,false)));
 
         public static final RegistryObject<Item> CAUCASUS_ZECTER_MASK = ITEMS.register("caucasus_zecter_mask",
-                () -> new RiderFormChangeItem(new Item.Properties(),0,"_mask","caucasus","caucasus_rider_belt",
+                () -> new RiderFormChangeItem(new Item.Properties(),0,"_masked","caucasus","zect_buckle_belt",
                         new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 1,true,false),
                         new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 40, 0,true,false)));
         public static final RegistryObject<Item> CAUCASUS_ZECTER = ITEMS.register("caucasus_zecter",
-                () -> new RiderFormChangeItem(new Item.Properties(),0,"","caucasus","caucasus_rider_belt",
+                () -> new RiderFormChangeItem(new Item.Properties(),0,"","caucasus","zect_buckle_belt",
                         new MobEffectInstance(MobEffects.DIG_SPEED, 40, 3,true,false),
                         new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 7,true,false),
                         new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 4,true,false),
@@ -143,27 +143,27 @@ public class Kabuto_Rider_Items {
                         new MobEffectInstance(MobEffects.NIGHT_VISION, 400, 0,true,false)).addSwitchForm(CAUCASUS_ZECTER_MASK.get()).AddToTabList(RiderTabs.KABUTO_TAB_ITEM));
         
         public static final RegistryObject<Item> HERCUS_ZECTER_MASK = ITEMS.register("hercus_zecter_mask",
-                () -> new RiderFormChangeItem(new Item.Properties(),0,"_mask","hercus","hercus_rider_belt",
+                () -> new RiderFormChangeItem(new Item.Properties(),0,"_masked","hercus","zect_buckle_belt",
                         new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 1,true,false),
                         new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 40, 0,true,false)));
         public static final RegistryObject<Item> HERCUS_ZECTER = ITEMS.register("hercus_zecter",
-                () -> new RiderFormChangeItem(new Item.Properties(),0,"","hercus","hercus_rider_belt",
+                () -> new RiderFormChangeItem(new Item.Properties(),0,"","hercus","zect_buckle_belt",
                         new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 1,true,false),
                         new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 3,true,false),
                         new MobEffectInstance(MobEffects.DIG_SPEED, 40, 3,true,false)).addSwitchForm(HERCUS_ZECTER_MASK.get()).AddToTabList(RiderTabs.KABUTO_TAB_ITEM));
         
         public static final RegistryObject<Item> KETAROS_ZECTER_MASK = ITEMS.register("ketaros_zecter_mask",
-                () -> new RiderFormChangeItem(new Item.Properties(),0,"_mask","ketaros","ketaros_rider_belt",
+                () -> new RiderFormChangeItem(new Item.Properties(),0,"_masked","ketaros","zect_buckle_belt",
                         new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 1,true,false),
                         new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 40, 0,true,false)));
         public static final RegistryObject<Item> KETAROS_ZECTER = ITEMS.register("ketaros_zecter",
-                () -> new RiderFormChangeItem(new Item.Properties(),0,"","ketaros","ketaros_rider_belt",
+                () -> new RiderFormChangeItem(new Item.Properties(),0,"","ketaros","zect_buckle_belt",
                         new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 1,true,false),
                         new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 3,true,false),
                         new MobEffectInstance(MobEffects.DIG_SPEED, 40, 3,true,false)).addSwitchForm(KETAROS_ZECTER_MASK.get()).AddToTabList(RiderTabs.KABUTO_TAB_ITEM));
         
         public static final RegistryObject<Item> LADY_ZECTER = ITEMS.register("lady_zecter",
-                () -> new RiderFormChangeItem(new Item.Properties(),0,"","lady","lady_rider_belt",
+                () -> new RiderFormChangeItem(new Item.Properties(),0,"","lady","zect_buckle_belt",
                         new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 1,true,false),
                         new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 3,true,false),
                         new MobEffectInstance(MobEffects.DIG_SPEED, 40, 3,true,false)).AddToTabList(RiderTabs.KABUTO_TAB_ITEM));
